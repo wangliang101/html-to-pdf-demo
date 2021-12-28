af3c2ed63840c46afb4e1777288367c093a61eae
wliang <wliang@infervision.com>
2021-12-29 01:11:01 +0800

    [add] init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import CanvasPage from './pages/canvasPage';
+import ImagePage from './pages/imagePage'
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app_wrap">
+        <Routes>
+            <Route path='/canvasPage' element={<CanvasPage />} />
+            <Route path='/imagePage' element={<ImagePage />} />
+            <Route path="*" element={<Navigate to="/imagePage"/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
